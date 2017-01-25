65fc05ab6c58f546a95e7440117bd4316e770846
Gábor Domonkos <gabor.domonkos@lombiq.com>
2017-01-25 21:57:28 +0100

    Adding MirroringConfigurationPartRecord for MirroringConfigurationPart to able to query based on the Status property
    --HG--
    branch : issue/LMBQ-34

---
+++ Handlers/MirroringConfigurationPartHandler.cs
@@ -0,0 +1,14 @@
+﻿using GitHgMirror.Common.Models;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+
+namespace GitHgMirror.Common.Handlers
+{
+    public class MirroringConfigurationPartHandler : ContentHandler
+    {
+        public MirroringConfigurationPartHandler(IRepository<MirroringConfigurationPartRecord> repository)
+        {
+            Filters.Add(StorageFilter.For(repository));
+        }
+    }
+}
