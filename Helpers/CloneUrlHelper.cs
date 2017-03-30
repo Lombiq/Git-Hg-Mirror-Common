d1bfe4562e77b7bd30c1f2c406f493962943ab46
Csanad Balla <csanad.balla@lombiq.com>
2017-03-30 21:56:16 +0200

    Adding helper for string operations
    --HG--
    branch : issue/LMBQ-50

---
+++ Helpers/CloneUrlHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace GitHgMirror.Common.Helpers
+{
+    public static class CloneUrlHelper
+    {
+        public static string ConvertToClickableUrl(string url)
+        {
+            var passwordOrTokenAdded = url.LastIndexOf('@') > 0;
+            url = passwordOrTokenAdded ? @"https:\\" + url.Substring(url.LastIndexOf('@') + 1) : url;
+
+            return url;
+        }
+
+        public static string GitUrlIsHgUrl(string url)
+        {
+            url = url.Remove(url.LastIndexOf(".git"));
+
+            if (url.StartsWith("git+"))
+            {
+                url = url.Substring("git+".Length);
+            }
+
+            return url;
+        }
+    }
+}
